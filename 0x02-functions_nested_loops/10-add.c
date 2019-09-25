27ad17cdc7a53c58263d959e6a1df08100899099
Jesus-Acevedo-Cano <Jeacevedocano@gmail.com>
2019-09-25 00:00:42 +0000

    to add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - to add numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+int c;
+c = (a + b);
+return (c);
+}

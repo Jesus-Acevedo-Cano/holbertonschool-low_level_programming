5c090795efe1b63d6985adf8f51f569cb34af722
Jesus-Acevedo-Cano <Jeacevedocano@gmail.com>
2019-09-26 22:12:40 +0000

    a # square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,6 +10,12 @@
 void print_square(int size)
 {
 int a, b;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
 for (a = 0 ; a < size; a++)
 {
 for (b = 0; b < size; b++)
@@ -17,5 +23,6 @@ for (b = 0; b < size; b++)
 _putchar('#');
 }
 _putchar('\n');
+}
 }
 }

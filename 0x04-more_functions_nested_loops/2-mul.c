ac64097879056945356333925e587a45dc26ee84
Jesus-Acevedo-Cano <Jeacevedocano@gmail.com>
2019-09-26 14:56:26 +0000

    multiply numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - to multiply numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+int c;
+c = (a * b);
+return (c);
+}

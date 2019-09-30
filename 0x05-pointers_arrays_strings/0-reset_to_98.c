f08901e7c9211e9736fc8fdd686f5664eb2a0ca6
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-09-30 23:10:40 +0000

    pointers and arrays begins here

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - to reset the value of a variable
+ * @n: for integers
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is the readme file to the tasks from the pointers and arrays 

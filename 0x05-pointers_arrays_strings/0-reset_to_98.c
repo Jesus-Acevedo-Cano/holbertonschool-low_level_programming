24cc613dc4406ff083e2dec04044d9f32ed1301e
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-01 17:55:06 +0000

    first pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - to reset the value of a variable
+ * reset_to_98 - pointer to reset the value of a variable
  * @n: for integers
  * Return: Always 0 (Success)
  */
 
 void reset_to_98(int *n)
 {
-
+*n=98;
 }

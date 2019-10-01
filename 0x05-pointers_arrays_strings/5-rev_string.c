df7e3df4e97722e1858adeb7e4b55d16279bbd21
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-01 23:39:07 +0000

    inverted lines

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_rev - function to print an array in reverse
+ * @s: for characters
+ * Return: Always 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+int count0 = 0;
+int count1 = 0;
+char a, b;
+while (s[count0] != '\0')
+{
+count0++;
+}
+count0--;
+for (count1 = 0; count1 < count0; count1++)
+{
+a = s[count0];
+b = s[count1];
+
+s[count0] = b;
+s[count1] = a;
+count0--;
+}
+}

09cadf4a1d2ef9ee7c6904e728ba33a6abd9aea1
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-03 22:36:50 +0000

    copy string arrays

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * _strncpy - function that copychar *_strncpy(char *dest, char *src, int n) strings
+ * _strncpy - function that copy strings
  * @dest: array elements
  * @src: array elements
  * @n: integer
@@ -16,5 +16,5 @@ for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
 for ( ; i < n; i++)
 dest[i] = '\0';
-return dest;
+return (dest);
 }

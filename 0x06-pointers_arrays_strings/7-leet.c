863ba13dcc8a57bd5e1c1ea2d755c149d58b36dc
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-05 17:32:34 +0000

    leets do it 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ **leet - function to change leters to numbers
+ *@a: string array
+ *
+ *Return: uppercase string
+ */
+char *leet(char *s)
+{
+	char b[] = "ol2ea56t";
+	int c, d;
+		c = 0;
+		while (s[c] != '\0')
+		{
+			d = 0;
+			while (b[d] != '\0')
+			{
+				if(s[c] == b[d] || s[c] == b[d] -32)
+				   s[c] = d + '0';
+				   d++;
+		    }
+			c++;
+		}
+return (s);
+_putchar('\n');
+}

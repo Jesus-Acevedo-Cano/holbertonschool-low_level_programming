cb4fcd82b6eeb8101f6c18723c45dd4b0bd6acc8
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-08 00:17:14 +0000

    compare bytes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: array elements
+ * @accept: replace char
+ *
+ * Return: number
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, b, c = 0;
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == ' ')
+			break;
+		else
+		{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+			c++;
+			}
+		}
+		}
+	}
+	return (c);
+}

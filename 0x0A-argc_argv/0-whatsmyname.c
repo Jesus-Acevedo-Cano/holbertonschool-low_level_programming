ba2704adabb940ab5aded97a975746d350dd1eb2
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-13 00:02:57 +0000

    initial files and 0 task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function, this time to print the name of the file
+ * @argc: arguments length
+ * @argv: arguments array
+ * Return: Always 0 (Success)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+file to de argc and argv exercises

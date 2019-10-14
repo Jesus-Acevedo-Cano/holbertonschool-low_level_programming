df6f1042052d256b91ad0b86f9bdc6e6190831c4
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-14 02:33:28 +0000

    adding the add task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,30 +8,30 @@
  * @argv: arguments array
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 int res = 0;
 int a;
 int b;
 
- if (argc < 2)
-   {
-     printf("0\n");
-     return (0);
-   }
-
- for (a = 1; a < argc; a++)
-   {
-     for (b = 0; argv[a][b] != '\0'; b++)
-       {
-	 if (argv[a][b] < 48 || argv[a][b] > 57)
-	   {
-	     printf("Error\n");
-	     return (1);
-	   }
-       }
-     res = res + atoi(argv[a]);
-   }
- printf("%d\n", res);
- return (0);
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}
+for (a = 1; a < argc; a++)
+{
+for (b = 0; argv[a][b] != '\0'; b++)
+{
+if (argv[a][b] < 48 || argv[a][b] > 57)
+{
+printf("Error\n");
+return (1);
+}
+}
+res = res + atoi(argv[a]);
+}
+printf("%d\n", res);
+return (0);
 }

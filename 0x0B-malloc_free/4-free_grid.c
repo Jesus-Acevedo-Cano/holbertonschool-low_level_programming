e0713bbe3c3d34a94697dfdc476e1b50f76c2d42
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-16 02:16:09 +0000

    adding files

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function that duplicates an array
+ * @grid: string array
+ * @height: string array
+ *
+ * Return: poiter
+ */
+
+void free_grid(int **grid, int height)
+{
+	char *a;
+	int d, c, b, e;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (c = 0; s1[c] != '\0'; c++)
+	{
+	}
+	for (d = 0; s2[d] != '\0'; d++)
+	{
+	}
+	a = malloc(sizeof(char) * (c + d + 1));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (b = 0; b <= c; b++)
+	{
+		a[b] = s1[b];
+	}
+	for (e = 0; e <= d; e++)
+	{
+		a[c + e] = s2[e];
+	}
+	return (a);
+}

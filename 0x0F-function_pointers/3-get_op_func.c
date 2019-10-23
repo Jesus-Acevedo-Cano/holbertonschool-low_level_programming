dd9b6273c72904a5df9dadddbab8a52c3285e9f3
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-23 22:32:45 +0000

    init files task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+BB#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <string.h>
+/**
+ * get_op_func - pointer to function needed by user
+ * @s: char
+ *
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+		return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+B#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * op_add - operator function
+ * @a: int
+ * @b: int
+ *
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - operator function
+ * @a: int
+ * @b: int
+ *
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - operator function
+ * @a: int
+ * @b: int
+ *
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - operator function
+ * @a: int
+ * @b: int
+ *
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - operator function
+ * @a: int
+ * @b: int
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

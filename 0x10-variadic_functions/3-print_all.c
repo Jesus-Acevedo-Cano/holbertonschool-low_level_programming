fc8b0cd32085e5b8bc85cd66b05f57514f3d14c5
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-26 23:56:27 +0000

    printing all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+/**
+ * print_string - Prints a string
+ * @str: String to print
+ * Return: Nothing
+ */
+
+void print_string(char *str)
+{
+	if (str == NULL)
+		printf("(nil)");
+	else
+		printf("%s", str);
+}
+/**
+ * print_all - Sample Text
+ * @format: Sample Text
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list in;
+	char use;
+	unsigned int i;
+
+	va_start(in, format);
+	i = 0;
+	while (*(format + i) != '\0')
+	{
+		use = 0;
+		switch (*(format + i))
+		{
+		case 'c':
+			printf("%c", va_arg(in, int));
+			use++;
+			break;
+		case 'i':
+			printf("%d", va_arg(in, int));
+			use++;
+			break;
+		case 'f':
+			printf("%f", va_arg(in, double));
+			use++;
+			break;
+		case 's':
+			print_string(va_arg(in, char *));
+			use++;
+			break;
+		}
+		if (*(format + i + 1) != '\0' && use == 1)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}

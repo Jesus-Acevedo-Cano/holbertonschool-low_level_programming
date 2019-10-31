ad3f9343d12ff004e90ffa327e8120ec5204eaba
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-10-31 21:01:48 +0000

    print list and number of nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[o] (nil)\n");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%i] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
 		h = h->next;

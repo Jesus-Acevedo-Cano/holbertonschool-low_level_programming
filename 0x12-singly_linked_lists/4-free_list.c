9e0d52751b2ea09c356349e4825b9c137505377b
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-11-01 01:15:29 +0000

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,8 @@
 
 void free_list(list_t *head)
 {
-	while (head != NULL)
-	{
-		if (head->str != NULL)
-		{
-			free(head->str);
-		}
+	if (head != NULL)
+		free(head->next)
+		free(head->str);
 		free(head);
-	}
 }

ef6946b698471c44f4a7c3b2d0a2fa877873ba21
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-11-05 19:41:45 +0000

    get de node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,15 +9,14 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	while (index > 0)
 	{
 		head = head->next;
 		index--;
 	}
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	else
 	return (head);
 }

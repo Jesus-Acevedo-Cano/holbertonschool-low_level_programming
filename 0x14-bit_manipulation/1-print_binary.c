71a0526fbe27bfb6bd29dc632b5b38fa489ca712
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-11-08 21:05:45 +0000

    adding files

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * print_binary - function to binary representation of a number
+ * @n: unsigned long integer
+ *
+ * Return: binary
+ */
+
+void print_binary(unsigned long int n)
+{
+
+}

c4e3eb9c50286669e85ba969c79273992886f772
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-11-08 01:50:07 +0000

    number of fliped bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - function to get the number of fliped bits
+ * @n: unsigned long integer
+ * @m: unsigned integer
+ *
+ * Return: value
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int a, b = 0;
+
+	a = m ^ n;
+	while (a != '\0')
+	{
+		b += a & 1;
+		a >>= 1;
+	}
+	return (b);
+}

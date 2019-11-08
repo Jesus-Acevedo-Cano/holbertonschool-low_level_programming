c87602ccbb7fbe4d1439b7ff91457f53913c1723
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-11-08 23:45:35 +0000

    delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			free(fakehead);
 			return (1);
 		}
-		for (count = 0; count < (index - 1); count++)
+		for (count = 0; count < index; count++)
 		{
 			some = fakehead;
 			if (!some)
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			fakehead = fakehead->next;
 			if (index == count)
 			{
-				fakehead = some->next;
+				fakehead->next = some->next;
 				free(some);
 				return (1);
 			}

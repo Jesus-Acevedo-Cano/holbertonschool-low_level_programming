ab41d78f1360cc568bf1bda33dda278f148ac4cd
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-11-13 00:06:31 +0000

    open doc and return number of letters

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,31 +19,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		doc = open(filename, O_RDONLY);
 		if (doc == -1)
 		{
-			return(0);
+			return (0);
 		}
 		mem = malloc(sizeof(char) * letters);
 		if (mem == NULL)
 		{
 			close(doc);
-			return(0);
+			return (0);
 		}
 		red = read(doc, mem, letters);
 		if (red < 0)
 		{
 			close(doc);
 			free(mem);
-			return(0);
+			return (0);
 		}
 		white = write(STDOUT_FILENO, mem, red);
 		if (white < 0)
 		{
 			close(doc);
 			free(mem);
-			return(0);
+			return (0);
 		}
 		close(doc);
 		free(mem);
-		return(red);
+		return (red);
 	}
-	return(0);
+	return (0);
 }

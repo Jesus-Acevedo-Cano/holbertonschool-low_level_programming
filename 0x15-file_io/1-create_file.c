82c3c905f0e5151ddd6b7434dbdce37cce3fee7f
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-11-13 00:34:10 +0000

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename != NULL)
 	{
-		doc= open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		doc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 		if (doc == -1)
 		{
 			return (-1);

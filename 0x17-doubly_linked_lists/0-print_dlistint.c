4852b180a3522791a6037f2f518e26970a67c947
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-12-13 19:28:58 +0000

    print list elements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function to print data structure
+ * @h: pointer to list
+ *
+ * Return: printed nodes cant
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n;
+
+	for(n = 0; h != NULL; n++)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+	}
+	return(n);
+}

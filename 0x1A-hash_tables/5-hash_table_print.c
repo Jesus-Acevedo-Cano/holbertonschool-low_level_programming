80a5d14d0eb77a54ac7b9ef155716649af98dabe
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-01-20 03:32:00 +0000

    print hash table values

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print values of hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int index;
+	hash_node_t *temp;
+	char *separator = "";
+
+	if (ht != NULL)
+	{
+		printf("{");
+
+		for (index = 0; index < ht->size; index++)
+		{
+			temp = ht->array[index];
+
+			while (temp)
+			{
+				printf("%s'%s': '%s'", separator, temp->key, temp->value);
+				separator = ", ";
+				temp = temp->next;
+			}
+
+		}
+		printf("}\n");
+	}
+}

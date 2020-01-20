a34d5d0a9564a042a92bfbeffbf77c990242aa8f
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-01-20 04:18:00 +0000

    delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete  hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int index;
+	hash_node_t *pos, *temp;
+
+	if (ht != NULL)
+	{
+		for (index = 0; index < ht->size; index++)
+		{
+			temp = ht->array[index];
+
+			while (temp)
+			{
+				pos = temp;
+				free(temp->key);
+				free(temp->value);
+				temp = temp->next;
+				free(pos);
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+	return;
+}

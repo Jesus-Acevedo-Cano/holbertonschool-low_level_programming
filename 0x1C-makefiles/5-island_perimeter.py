dc8ef8a6ab1e125da5413816e70c4d759c137aee
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-02-21 20:36:40 +0000

    fixing 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,14 +6,15 @@ def island_perimeter(grid):
     """function to find island perimeter"""
     count = 0
     edge = 0
-    for i in range(len(grid)-1):
-        for j in range(len(grid[i]) -1):
+    for i in range(len(grid) - 1):
+        for j in range(len(grid[i]) - 1):
             if grid[i][j] == 1:
                 count += 1
-            if grid[i][j] == 1 and grid[i][j + 1] == 1:
-                edge += 1
-            elif grid[i][j] == 1 and grid[i + 1][j] == 1:
-                edge += 1
+                if j < (len(grid[i]) -1) and i < (len(grid) - 1):
+                    if grid[i][j] == 1 and grid[i][j + 1] == 1:
+                        edge += 1
+                    if grid[i][j] == 1 and grid[i + 1][j] == 1:
+                        edge += 1
     count = count * 4
     edge = edge * 2
     perimeter = count - edge

9eb015095fc3cc45f189027a0d2a927f0a77d90b
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-02-21 20:38:20 +0000

    fixing 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,11 +10,10 @@ def island_perimeter(grid):
         for j in range(len(grid[i]) - 1):
             if grid[i][j] == 1:
                 count += 1
-                if j < (len(grid[i]) -1) and i < (len(grid) - 1):
-                    if grid[i][j] == 1 and grid[i][j + 1] == 1:
-                        edge += 1
-                    if grid[i][j] == 1 and grid[i + 1][j] == 1:
-                        edge += 1
+                if grid[i][j] == 1 and grid[i][j + 1] == 1:
+                    edge += 1
+                if grid[i][j] == 1 and grid[i + 1][j] == 1:
+                    edge += 1
     count = count * 4
     edge = edge * 2
     perimeter = count - edge
